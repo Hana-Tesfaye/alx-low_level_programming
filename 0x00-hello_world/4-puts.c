72d5aebea37ff04a08c3cb17a7ba77418cdb67db
Hana-Tesfaye <tesfayehanna69@gmail.com>
2022-12-09 15:32:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,22 +2,16 @@
 
 
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+/** 
+ * main - prints "Programming is like building a multilingual
+ *puzzle, followed by a new line.
+ * return 0.
+ */
 
 int main(void)
 
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
