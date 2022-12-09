4526c1dc2a6949483c784e6e4dcd331328ca6a1c
Hana-Tesfaye <tesfayehanna69@gmail.com>
2022-12-09 15:42:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 
-
 /** 
  * main - prints "Programming is like building a multilingual
  *puzzle, followed by a new line.
- * return 0.
+ * Return: Always 0.
  */
 
 int main(void)
