99811493f40e76ceb55b2417ca617a89b0cf8437
Hana-Tesfaye <tesfayehanna69@gmail.com>
2022-12-13 11:49:04 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,37 @@
 #include "main.h"
 
 /**
- * main -print _putchar
- * is written by hana
- *Return: 0
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ **/
 
 int main(void)
+
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('/n');
-return (0);
+
+		char *sh = "_putchar";
+
+
+
+			while (*sh)
+
+					{
+
+								_putchar(*sh);
+
+										sh++;
+
+											}
+
+				_putchar('\n');
+
+
+
+					return (0);
+
 }
